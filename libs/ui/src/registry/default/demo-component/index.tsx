64f26fedc2659ee17d79afa9e34b5e07d412f860
David Höck <d.hoeck@elevantiq.com>
2025-07-18 14:36:23 +0200

    chore: Established a working PoC for colocating shadcn component
    - Colocated components with apps in the same monorepo
    - TS paths are the same as in a default setup of Shadcn

---
+++ libs/ui/src/registry/default/demo-component/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export function DemoComponent() {
+  return (
+    <div>
+      Demo Component
+      <Button>Click me</Button>
+    </div>
+  );
+}
